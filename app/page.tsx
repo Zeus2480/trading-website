2d2306c8762fb538f76fb4d8aed2f4cd03402136
Faizan <siddiquifaizan2480@gmail.com>
2024-12-23 22:39:04 +0530

    FIrst commit

---
+++ app/page.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Hero from "@/components/Hero";
+import Hero2 from "@/components/Hero2";
+import Explosion from "@/components/Explosion";
+import Navbar from "@/components/Navbar";
+import ScrollSection from "@/components/ScrollSection";
+import CardGridSection from "@/components/CardGridSection";
+import MarqueeCardSection from "@/components/MarqueeCardSection";
+import MarqueeSlider from "@/components/MarqueeSlider";
+
+import Pricing from "@/components/Pricing";
+import FAQs from "@/components/FAQs";
+import SpotlightHero from "@/components/SpotlightHero";
+import Footer from "@/components/Footer";
+
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <SpotlightHero/>
+      <ScrollSection/>
+      <CardGridSection/>
+      <MarqueeSlider />
+      <MarqueeCardSection/>
+      <Pricing/>
+      <FAQs/>
+      <Footer/>
+    </>
+  );
+}
